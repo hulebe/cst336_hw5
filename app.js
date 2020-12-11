261bcd2ea138d89958aa17cf36238b3cc7ca9fbe
EC2 Default User <ec2-user@ip-172-31-24-95.us-west-1.compute.internal>
2020-12-11 10:00:57 +0000

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app     = express();
+const fetch   = require("node-fetch");
+
+app.set("view engine", "ejs");
+
+//routes
+app.get("/", async function(req, res, next){
+    let apiUrl = `https://official-joke-api.appspot.com/random_joke`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+	res.render("index", {"randomJoke": data.setup, "jokeAnswer": data.punchline});
+	next();
+});
+
+app.get("/search", async function(req, res){
+    let keyword = "";
+    if (req.query.keyword) {
+        keyword = req.query.keyword;
+    }
+    
+    let apiUrl = `https://official-joke-api.appspot.com/jokes/${keyword}/ten`;
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    
+    res.render("results", {"joke": data[0].setup, "jokeAnswer":data[0].punchline});
+});
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Express server is running...");
+});
